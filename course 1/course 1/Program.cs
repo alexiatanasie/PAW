4390ca37d33eb5e468f2649d30122584aa4dd861
Alexia <150076398+alexiatanasie@users.noreply.github.com>
2024-03-13 21:59:57 +0200

    HelloWorld

---
+++ course 1/course 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; //referenced namespace
+
+namespace NameSpaceProgram
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //HelloWorld application
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        } //end main
+    }//end class
+} //end namespace
